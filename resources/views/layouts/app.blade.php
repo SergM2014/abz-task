c93dd7455c425270ceafa6b1986d548ee9a2d6f0
SergM2014 <weisse011@gmail.com>
2022-12-01 21:36:39 +0200

    Employee Controller and routes +

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,10 +28,14 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                @auth
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                         <li class="nav-item">
+                            <a class="nav-link" href="/admin/employee">Employee</a>
+                        </li>
                     </ul>
+                @endauth
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
