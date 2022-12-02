1e15e6aa0699b183bb08d75830e3ee06fe39a13b
SergM2014 <weisse011@gmail.com>
2022-12-02 20:27:43 +0200

    create employee input forms restriction +

--- resources/views/admin/employees/create.blade.php
+++ resources/views/admin/employees/create.blade.php
@@ -5,20 +5,20 @@
 <h2> Create new Employee </h2>
 
 <form class="m-3">
-  <div class="form-group">
+  <div class="form-group" method="post" action=" {{ route('employees.store') }}>
     <label for="firstName">First Name</label>
     <input type="text" class="form-control" name="firstName" id="firstName" placeholder="First Name"
-    >
+    minlength="2" maxlength="256" required >
   </div>
   <div class="form-group">
     <label for="middleName">Middle Name</label>
     <input type="text" class="form-control" name="middleName" id="middleName" placeholder="Middle Name"
-    >
+    minlength="2" maxlength="256" >
   </div>
   <div class="form-group">
     <label for="lastName">Last Name</label>
     <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Last Name"
-    >
+    minlength="2" maxlength="256" required  >
   </div>
   <div class="form-group">
     <label for="positionId">Position select</label>
@@ -42,15 +42,16 @@
   </div>
   <div class="form-group">
     <label for="phone">Phone</label>
+    <div class="text-red">
+    <smal>Put phone only in following formats: +38(066)1234567 +38(0661)123456 +38(06612)12345 1234567</smal>
+    </div>
     <input type="tel" class="form-control" name="phone" id="phone" placeholder="Phone"
     pattern="([\+]\d{2}[\(]\d{3}[\)]\d{7})|([\+]\d{2}[\(]\d{4}[\)]\d{6})|([\+]\d{2}[\(]\d{5}[\)]\d{5})|\d{10}"
-    required
-    >
-    ([\+] \d{2} [\(] \d{5} [\)] \d{5}) | \d{10
+    required >
   </div>
   <div class="form-group">
     <label for="salary">Salary</label>
-    <input type="text" class="form-control" name="salary" id="salary" placeholder="Salary">
+    <input type="text" class="form-control" name="salary" id="salary" placeholder="Salary" required >
   </div>
   <div class="form-group">
     <label for="photo">Photo</label>
