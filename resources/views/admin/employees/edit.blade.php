e4de2940a1e93cbf753e70b793b2fc9b5eecab7d
SergM2014 <weisse011@gmail.com>
2022-12-05 16:06:42 +0200

    uploadPhoto segment is separated to a file

--- resources/views/admin/employees/edit.blade.php
+++ resources/views/admin/employees/edit.blade.php
@@ -81,35 +81,8 @@
      <div class="invalid-feedback"><?= $errors->first('salary') ?></div>
   </div>
   
-  <!-- upload file section -->
-  <div class="col-md-12 col-sm-12 col-xs-12" id="imgBlock">
-  <label for="salary">Photo upload</label>
-         <input type="hidden" name="photo" id="employeePhoto">
-        <!-- File preview --> 
-        <div id="filepreview" class="mb-3" > 
-          <img src="{{ asset('storage/uploads/thumbs/'.$photo) }}" class="rounded border border-primary" with="150px" height="150px">
-          
-         </div>
-
-         <div class="my-3 d-none" id="rotateControlBlock">
-               <button type="button" class="btn btn-info btn-sm" id="rotateLeft">Rotate Left</button>
-               <button type="button" class="btn btn-info btn-sm" id="rotateRight">Rotate Right</button>
-            </div>
-
-        <div class="form-group">
-
-            <input type='file' id="image" name='image' class="form-control" >
-              
-        </div>
-
-        <div class="form-group">
-           <div class="col-md-6">
-              <input type="button" id="uploadImage" value='Upload Image' class='btn btn-success'>
-              <button type="button" class="btn btn-danger" id="deleteImage" >Delete Image</button>
-           </div>
-        </div>
-      </div>      
-
+  @include('admin.employees.partials.uploadPhoto')
+     
     <div class="text-center">
       <button type="submit" class="btn btn-primary mb-2 btn-lg">Update User</button>
     </div>
