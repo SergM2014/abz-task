7e57f6e22a3edadb79ad461ad28a2f8c39ae7813
SergM2014 <weisse011@gmail.com>
2022-12-06 13:30:14 +0200

    js livewire is added

--- resources/views/admin/employees/index.blade.php
+++ resources/views/admin/employees/index.blade.php
@@ -24,4 +24,13 @@
     <!-- /.content -->
   
   <!-- /.content-wrapper -->
+<script>
+  Livewire.on('laraveltable:action:confirm', (actionType, actionIdentifier, modelPrimary, confirmationQuestion) => {
+    // You can replace this native JS confirm dialog by your favorite modal/alert/toast library implementation. Or keep it this way!
+    if (window.confirm(confirmationQuestion)) {
+        // As explained above, just send back the 3 first argument from the `table:action:confirm` event when the action is confirmed
+        Livewire.emit('laraveltable:action:confirmed', actionType, actionIdentifier, modelPrimary);
+    }
+});
+</script>
   @endsection
