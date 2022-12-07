3c3113e482812039fd7cde7c22bf8ae6a73e43f7
SergM2014 <weisse011@gmail.com>
2022-12-07 14:56:06 +0200

    blade if else has leader siblings +

--- resources/views/admin/employees/changeLeader.blade.php
+++ resources/views/admin/employees/changeLeader.blade.php
@@ -3,24 +3,28 @@
 @section('content')
 
 @if ($siblingsNumber<1)
-<h1>!!!!!!!!!!!!!</h1>
-@endif
-<h2>Hallo this is changeLeader</h2>
-<h3>the Leader, that You want to delete is: {{ $leader->first_name.' '.$leader->middle_name.' '.$leader->last_name }}</h3>
-<p>He has a position: {{ $leader->position_id }}</p>
-<p>But before deletion of the leader, You should pick up another Leader!</p>
-  <div class="form-group">
-    <label for="leaderId">Select another Leader</label>
-    <input type="hidden"  id="positionId" value="{{ old('leaderId')?? $leader->position_id }}" >
-    <input type="hidden" name="employeeId" id="employeeId" value="{{ old('employeeId')?? $leader->id }}" > 
-    <select name="leaderId" id="anotherLeaderIdSelect" class="form-control <?= $errors->has('leaderId')? 'is-invalid' : '' ?> select2" ></select>
-    <div class="invalid-feedback"><?= $errors->first('leaderId') ?></div>
-  </div>
+    <h1>The current mployee {{ $leader->first_name.' '.$leader->middle_name.' '.$leader->last_name }} #{{ $leader->id}}</h1>
+    <p class="text-red">doesnot have siblings, that's why it is impossible to be deleted! </p>
+    <p>Please return back!</p>
+@else
+    <h2>Hallo this is changeLeader</h2>
+    <h3>the Leader, that You want to delete is: {{ $leader->first_name.' '.$leader->middle_name.' '.$leader->last_name }}</h3>
+    <p>He has a position: {{ $leader->position_id }}</p>
+    <p>But before deletion of the leader, You should pick up another Leader!</p>
+    <p>You can choose among {{ $siblingsNumber }} other colleges on the given position</p>
+    <div class="form-group">
+        <label for="leaderId">Select another Leader</label>
+        <input type="hidden"  id="positionId" value="{{ old('leaderId')?? $leader->position_id }}" >
+        <input type="hidden" name="employeeId" id="employeeId" value="{{ old('employeeId')?? $leader->id }}" > 
+        <select name="leaderId" id="anotherLeaderIdSelect" class="form-control <?= $errors->has('leaderId')? 'is-invalid' : '' ?> select2" ></select>
+        <div class="invalid-feedback"><?= $errors->first('leaderId') ?></div>
+    </div>
 
-  <h2>It is a list of {{ count($subOrdinates)}} employees, that should changeLeader</h2>
-  @foreach( $subOrdinates as $subOrdinate)
-    {{ $subOrdinate->first_name.' '.$subOrdinate->middle_name.' '.$subOrdinate->last_name }} <br>
+    <h2>It is a list of {{ count($subOrdinates)}} employees, that should changeLeader</h2>
+    @foreach( $subOrdinates as $subOrdinate)
+        {{ $subOrdinate->first_name.' '.$subOrdinate->middle_name.' '.$subOrdinate->last_name }} <br>
 
-  @endforeach
+    @endforeach
 
+  @endif
 @endsection
