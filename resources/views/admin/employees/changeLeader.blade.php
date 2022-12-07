d5545616771435e1e7f0cf738ff339c2a22d8e3d
SergM2014 <weisse011@gmail.com>
2022-12-07 15:03:59 +0200

    return back btn to change leader form +

--- resources/views/admin/employees/changeLeader.blade.php
+++ resources/views/admin/employees/changeLeader.blade.php
@@ -2,13 +2,17 @@
 
 @section('content')
 
+<a href="{{ route('employees.index')}}" class="btn btn-danger">Revert deleting & go back</a>
+
+
 @if ($siblingsNumber<1)
     <h1>The current mployee {{ $leader->first_name.' '.$leader->middle_name.' '.$leader->last_name }} #{{ $leader->id}}</h1>
     <p class="text-red">doesnot have siblings, that's why it is impossible to be deleted! </p>
     <p>Please return back!</p>
+    
 @else
-    <h2>Hallo this is changeLeader</h2>
-    <h3>the Leader, that You want to delete is: {{ $leader->first_name.' '.$leader->middle_name.' '.$leader->last_name }}</h3>
+    <h2>Hallo this is Change Leader form</h2>
+    <h3>The curren Leader, that You want to delete is: {{ $leader->first_name.' '.$leader->middle_name.' '.$leader->last_name }}</h3>
     <p>He has a position: {{ $leader->position_id }}</p>
     <p>But before deletion of the leader, You should pick up another Leader!</p>
     <p>You can choose among {{ $siblingsNumber }} other colleges on the given position</p>
