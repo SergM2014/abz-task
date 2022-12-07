fc5b6d627da0f20179fa66ea46fc38021ea6cb1e
SergM2014 <weisse011@gmail.com>
2022-12-07 21:16:09 +0200

    selected opinion old input for create form +

--- resources/views/admin/employees/create.blade.php
+++ resources/views/admin/employees/create.blade.php
@@ -34,9 +34,7 @@
     <select class="form-control <?= $errors->has('positionId')? 'is-invalid' : '' ?>"
      id="positionId"  name="positionId">
      @foreach  ($positions as $position)
-      <option value= "{{ $position->id }}" 
-     
-      >{{ $position->title }}</option>
+      <option {{ old('positionId') == $position->id ? "selected" : "" }} value= "{{ $position->id }}" >{{ $position->title }}</option>
       @endforeach
     </select>
     <div class="invalid-feedback"><?= $errors->first('positionId') ?></div>
@@ -59,7 +57,7 @@
   <div class="form-group">
     <label for="phone">Phone</label>
     <div class="text-red">
-    <small>Put phone only in following formats: +38(066)1234567 +38(0661)123456 +38(06612)12345 1234567</small>
+    <small>Put phone only in following formats: +38(066)1234567 +38(0661)123456 +38(06612)12345 5654995</small>
     </div>
     <input type="tel" class="form-control <?= $errors->has('phone')? 'is-invalid' : '' ?>" 
     name="phone" id="phone" value="{{ old('phone') }}" placeholder="Phone"
